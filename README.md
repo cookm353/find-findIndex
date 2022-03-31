3b676f963c140c3af5b344f3c1680c0de0e966aa
Matthew Cook <cookm353@gmail.com>
2022-03-31 16:19:50 -0400

    finishing test-compliant script

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # find-findIndex
 
 Exercises using find() and findIndex()
+
+
+## `find` and `findIndex`
+* `find()` finds the first element in an array that meets a condition
+* Returns the first value if found or undefined if value isn't found
+```js
+const scores = [0, 0, 0, 0, 68, 75, 83, 85, 89, 93, 95]
+
+scores.find(function(score) {
+	return score > 75
+})
+// 83
+```
+* `findIndex()` is essentially the same as `find()`, only it returns the index instead
+```js
+const scores = [0, 0, 0, 0, 68, 75, 83, 85, 89, 93, 95]
+
+scores.findIndex(function(score) {
+	return score > 0 && score % 2 === 0
+})
+// 4
+```
--- find-findindex.js
+++ find-findindex.js
@@ -11,11 +11,17 @@ findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
 findUserByUsername(users, 'taco') // undefined
 */
 
-
-function findUserByUsername(usersArray, username) {}
+// Done!
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(function(user){
+    return user["username"] === username
+  })
+}
 
 /*
-Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
+Write a function called `removeUser` which accepts an array of objects, each with a key of username, 
+and a string. The function should remove the object from the array. 
+If the object is not found, return undefined. 
 
 const users = [
   {username: 'mlewis'},
@@ -27,4 +33,14 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+	const userIndex =  usersArray.findIndex(function(user) {
+		return user["username"] === username
+	})
+	
+	if (userIndex !== -1) {
+		return usersArray.splice(userIndex, 1)[0]
+	} else {
+		return undefined
+	}
+}
